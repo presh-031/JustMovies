923e2746827f835d26e0b55441594e4f620a70c9
presh-031 <akingbulugbe.dev@gmail.com>
2022-12-15 11:44:06 +0100

    testing out styles on deployed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,53 +1,45 @@
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 import API_REQUESTS from "../api/requests";
-
-import Slider from "react-slick";
 
 const Hero = () => {
   const { isLoading, error, data } = useQuery("popular-movies", () =>
     fetch(API_REQUESTS.Popular).then((res) => res.json())
   );
-  console.log(data);
 
-  // settings to configure slider
-  const settings = {
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: true,
-  };
+  useEffect(() => {
+    const random = Math.floor(Math.random() * data?.results?.length);
+    const randomMovie = data?.results[random];
+
+    console.log("randomMovie", randomMovie);
+  }, [data]);
+
+  // console.log(data);
+  // const random = Math.floor(Math.random() * data?.results?.length);
+  // const randomMovie = data?.results[random];
 
   return (
-    <div className="h-screen  relative">
-      <Slider {...settings}>
-        {data?.results?.map((popularMovie) => {
-          return (
-            <>
-              <img
-                className="h-full object-cover w-full"
-                src="https://image.tmdb.org/t/p/original/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg"
-                alt=""
-              />
-              <div className="text-white absolute  top-44 left-8">
-                <p className="font-semibold text-5xl mb-4">Warriors of Future</p>
-                <div className="flex gap-4">
-                  <button className="py-4 px-8  text-2xl rounded-sm hover:border-[1px] hover:bg-transparent hover:border-white hover:outline-none hover:text-white text-black bg-slate-300">
-                    Play
-                  </button>
-                  <button className="py-4 px-8 border-[1px] rounded-sm border-white text-2xl">Watch Later</button>
-                </div>
-                <p className="mt-12 mb-8 text-2xl text-gray-400">Released: 2022-08-05</p>
-                <p className="text-2xl">
-                  When a meteor carrying a destructive plant strikes the world, a suicide squad is given hours to save
-                  their post-apocalyptic city from total collapse.
-                </p>
-              </div>
-            </>
-          );
-        })}
-      </Slider>
+    <div className="relative  h-screen">
+      <div className="absolute h-full w-full bg-gradient-to-t from-black"></div>
+
+      <div className="h-full">
+        <img
+          className="h-full w-full object-cover"
+          src={`https://image.tmdb.org/t/p/original${randomMovie?.backdrop_path}`}
+          alt=""
+        />
+        <div className="absolute top-44  left-8 text-white">
+          <p className="mb-4 text-5xl font-semibold">{randomMovie?.title || randomMovie?.original_title || "Title"}</p>
+          <div className="flex gap-4">
+            <button className="rounded-sm bg-slate-300  py-4 px-8 text-2xl text-black hover:border-[1px] hover:border-white hover:bg-transparent hover:text-white hover:outline-none">
+              Play
+            </button>
+            <button className="rounded-sm border-[1px] border-white py-4 px-8 text-2xl">Watch Later</button>
+          </div>
+          <p className="mt-12 mb-8 text-2xl text-gray-400">Released: 2022-08-05</p>
+          <p className="text-2xl">{randomMovie?.overview} </p>
+        </div>
+      </div>
     </div>
   );
 };
